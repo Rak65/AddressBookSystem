3e6993a08038cc1d70d69e392a8533d30860ebc2
Rak65 <rakeshp6505@gmail.com>
2023-05-07 19:59:33 +0530

    Address Book System

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System.");
+            Console.ReadKey();
+        }
+    }
+}
