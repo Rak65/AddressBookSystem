ced996a5a3cd31328a0860c667871b2647d14754
Rak65 <rakeshp6505@gmail.com>
2023-05-11 22:10:14 +0530

    Add new contact to Address Book

---
+++ AddressBookSystem/Contact.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class Contact
+    {
+        private String firstName;
+        private String lastName;
+        private String city;
+        private String state;
+        private String zip;
+        private String phoneNumber;
+        private String email;
+
+        public void setFirstName(String firstName)
+        {
+            this.firstName = firstName;
+        }
+        public String getFirstName()
+        {
+            return firstName;
+        }
+
+        public void setLastName(String lastName)
+        {
+            this.lastName = lastName;
+        }
+        public String getLastName()
+        {
+            return lastName;
+        }
+
+        public void setCity(String city)
+        {
+            this.city = city;
+        }
+        public String getCity()
+        {
+            return city;
+        }
+
+        public void setState(String state)
+        {
+            this.state = state;
+        }
+        public String getState()
+        {
+            return state;
+        }
+
+        public void setZip(String zip)
+        {
+            this.zip = zip;
+        }
+        public String getZip()
+        {
+            return zip;
+        }
+
+        public void setPhoneNumber(String phoneNumber)
+        {
+            this.phoneNumber = phoneNumber;
+        }
+        public String getPhoneNumber()
+        {
+            return phoneNumber;
+        }
+
+        public void setEmail(String email)
+        {
+            this.email = email;
+        }
+        public String getEmail()
+        {
+            return email;
+        }
+        public void Add_Contact(List<Contact> lis)
+        { 
+            Contact P1 = new Contact();
+            Console.WriteLine("Enter First Name: ");
+            P1.setFirstName(Console.ReadLine());
+            Console.WriteLine("Enter Last Name : ");
+            P1.setLastName(Console.ReadLine());
+            Console.WriteLine("Enter City : ");
+            P1.setCity(Console.ReadLine());
+            Console.WriteLine("Enter State : ");
+            P1.setState(Console.ReadLine());
+            Console.WriteLine("Enter zip : ");
+            P1.setZip(Console.ReadLine());
+            Console.WriteLine("Enter PhoneNumber: ");
+            P1.setPhoneNumber(Console.ReadLine());
+            Console.WriteLine("Enter Email ID : ");
+            P1.setEmail(Console.ReadLine());
+            lis.Add(P1);
+        }
+        public void DisplayContacts(List<Contact> lis) {
+
+            Console.WriteLine("Contacts in Address Book:");
+            foreach (Contact contact in lis)
+            {
+                Console.WriteLine("Name: {0} {1}", contact.firstName, contact.lastName);
+                Console.WriteLine("City: {0}", contact.city);
+                Console.WriteLine("State: {0}", contact.state);
+                Console.WriteLine("Zip: {0}", contact.zip);
+                Console.WriteLine("Phone Number: {0}", contact.phoneNumber);
+                Console.WriteLine("Email: {0}", contact.email);
+                Console.WriteLine();
+            }
+        }       
+
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,18 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AddressBookSystem
 {
-    public class Program
+    class AddressBookSystem
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            ContactInfo addressBook = new ContactInfo();
-            addressBook.CreateContact();
+            List<Contact> lis = new List<Contact>();
+            int option;
+            do
+            {
+                Contact contact = new Contact();
+                Console.WriteLine("Choose your option :");
+                Console.WriteLine("1.Add Contact");
+                Console.WriteLine("2.Display all Contact.");
+                option =Convert.ToInt32(Console.ReadLine());
+                
+                switch (option)
+                {
+                    case 1:
+                        contact.Add_Contact(lis );
+                        break;
+                    case 2:
+                        contact.DisplayContacts(lis );
+                        break;
+                    default :
+                        Console.WriteLine("Enter valid option :");
+                        break;
+                }Console.WriteLine("Do you want to continue (1.Continue 0.Exit)");
+                option = Convert.ToInt32(Console.ReadLine());
+            }while (option != 0);
+
             Console.ReadKey();
         }
     }
+    
 }
